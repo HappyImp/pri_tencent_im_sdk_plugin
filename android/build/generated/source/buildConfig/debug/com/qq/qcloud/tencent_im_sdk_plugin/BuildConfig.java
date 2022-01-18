819c9c2614a1efeec17e4213a136ab34cab6912f
kyluzoi <kyluzoi@gmail.com>
2022-01-18 11:24:34 +0800

    init

---
+++ android/build/generated/source/buildConfig/debug/com/qq/qcloud/tencent_im_sdk_plugin/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.qq.qcloud.tencent_im_sdk_plugin;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.qq.qcloud.tencent_im_sdk_plugin";
+  public static final String BUILD_TYPE = "debug";
+}
